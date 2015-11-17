fe80ead1a6dd517c97003a7a8754e21e38b95fe6
ramon <ramon@ramon-Aspire-E5-571>
2015-11-17 16:34:10 +0100

    Segundo commit

--- Prueba/src/Prueba.java
+++ Prueba/src/Prueba.java
@@ -1,10 +1,13 @@
-
 public class Prueba {
 
 	public static void main(String[] args) {
-		try{
-			System.out.println("Hola");
-		} catch(Exception e){
+		try {
+			try {
+				System.out.println("Hola");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
