ed5270070043c8167edb141bcb47c76dc3eb130d
ramon <ramon@ramon-Aspire-E5-571>
2015-11-17 16:58:55 +0100

    Subido proyecto Maven

---
+++ aleteo.digital.prueba/src/main/java/aleteo/digital/prueba/Prueba.java
@@ -0,0 +1,13 @@
+package aleteo.digital.prueba;
+
+public class Prueba {
+	
+	public static int multiplica(){
+		return 2*2;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(multiplica());
+	}
+
+}
